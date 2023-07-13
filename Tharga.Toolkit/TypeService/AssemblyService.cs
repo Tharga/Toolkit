92ac4a095fc388dc2d289ae621c6a77121116405
Daniel Bohlin <daniel.bohlin@gmail.com>
2023-07-13 13:06:16 +0200

    add static methods

--- Tharga.Toolkit/TypeService/AssemblyService.cs
+++ Tharga.Toolkit/TypeService/AssemblyService.cs
@@ -6,40 +6,80 @@ using System.Reflection;
 
 namespace Tharga.Toolkit.TypeService;
 
-internal class AssemblyService : IAssemblyService
+public class AssemblyService : IAssemblyService
 {
     private static readonly ConcurrentDictionary<string, TypeInfo[]> Cache = new();
 
-    public void LoadTypes(string cacheKey, Func<TypeInfo, bool> predicate, IEnumerable<Assembly> assemblies = null)
+    internal AssemblyService()
     {
-        var data = GetCurrentDomainDefinedTypes(assemblies)
-            .Where(x => x.FullName != null)
-            .Where(predicate)
-            .ToArray();
+    }
 
+    public void LoadTypes(string cacheKey, Func<TypeInfo, bool> filter, IEnumerable<Assembly> assemblies = null)
+    {
+        var data = GetTypes(filter, assemblies).ToArray();
         Cache.AddOrUpdate(cacheKey, data, (_, _) => data);
     }
 
-    public TypeInfo[] GetTypes(string cacheKey, Func<TypeInfo, bool> predicate = null)
+    public TypeInfo[] GetTypes(string cacheKey, Func<TypeInfo, bool> filter = null)
     {
         if (Cache.TryGetValue(cacheKey, out var data))
         {
             return data;
         }
 
-        if (predicate != null)
+        if (filter != null)
         {
-            LoadTypes(cacheKey, predicate);
+            LoadTypes(cacheKey, filter);
             if (Cache.TryGetValue(cacheKey, out data))
             {
                 return data;
             }
         }
 
-        throw new InvalidOperationException($"Cannot find any loaded types for '{cacheKey}'. Call LoadTypes on startup or provide a predicate.");
+        throw new InvalidOperationException($"Cannot find any loaded types for '{cacheKey}'. Call {nameof(LoadTypes)} on startup or provide a {nameof(filter)}.");
     }
 
-    internal static Assembly[] GetAssemblies(IEnumerable<Assembly> assemblies)
+    /// <summary>
+    /// Get all assemblies in the current app domain based on the first part of the namespace from the EntryAssembly or ExecutingAssembly.
+    /// Example. 'Namespace [FirstPart].[SecondPart] will return all assemblies that starts with [FirstPart].'
+    /// </summary>
+    /// <returns></returns>
+    public static IEnumerable<Assembly> GetAssemblies()
+    {
+        return GetAssemblies(null);
+    }
+
+    /// <summary>
+    /// Get all types based on the provided type and parameters.
+    /// </summary>
+    /// <typeparam name="TBaseType">Type, basetype of interface.</typeparam>
+    /// <param name="filter">Filter for what types to return.</param>
+    /// <param name="assemblies">Assemblies where to find the types. See 'GetAssemblies' for default behaviour.</param>
+    /// <returns></returns>
+    public static IEnumerable<TypeInfo> GetTypes<TBaseType>(Func<TypeInfo, bool> filter = null, IEnumerable<Assembly> assemblies = null)
+    {
+        var assms = GetAssemblies(assemblies);
+        var types = assms.SelectMany(x => x.DefinedTypes)
+            .Where(x => x.IsOfType<TBaseType>())
+            .Where(x => filter?.Invoke(x) ?? true);
+        return types;
+    }
+
+    /// <summary>
+    /// Get all types for provided parameters.
+    /// </summary>
+    /// <param name="filter">Filter for what types to return.</param>
+    /// <param name="assemblies">Assemblies where to find the types. See 'GetAssemblies' for default behaviour.</param>
+    /// <returns></returns>
+    public static IEnumerable<TypeInfo> GetTypes(Func<TypeInfo, bool> filter = null, IEnumerable<Assembly> assemblies = null)
+    {
+        var assms = GetAssemblies(assemblies);
+        var types = assms.SelectMany(x => x.DefinedTypes)
+            .Where(x => filter?.Invoke(x) ?? true);
+        return types;
+    }
+
+    private static IEnumerable<Assembly> GetAssemblies(IEnumerable<Assembly> assemblies)
     {
         if (assemblies != null) return assemblies.ToArray();
 
@@ -51,45 +91,5 @@ internal class AssemblyService : IAssemblyService
             .ToArray();
 
         return new[] { current }.Union(assms).ToArray();
-    }
-
-    internal static TypeInfo[] GetCurrentDomainDefinedTypes(IEnumerable<Assembly> assemblies)
-    {
-        var assms = GetAssemblies(assemblies);
-        var types = assms.SelectMany(x => x.DefinedTypes).ToArray();
-        return types;
-    }
-
-    internal static TypeInfo[] GetCurrentDomainDefinedTypes<T>(IEnumerable<Assembly> assemblies, bool includeInterfaces = false, bool includeAbstract = false)
-    {
-        var assms = GetAssemblies(assemblies);
-        var types = assms.SelectMany(x => x.DefinedTypes)
-            .Where(x => x.IsOfType(typeof(T)))
-            .Where(x =>
-            {
-                if (!x.IsInterface && !x.IsAbstract)
-                {
-                    return true;
-                }
-
-                if (includeInterfaces && includeAbstract)
-                {
-                    return true;
-                }
-
-                if (includeInterfaces && x.IsInterface)
-                {
-                    return true;
-                }
-
-                if (includeAbstract && x.IsAbstract && !x.IsInterface)
-                {
-                    return true;
-                }
-
-                return false;
-            })
-            .ToArray();
-        return types;
     }
 }
--- Tharga.Toolkit/TypeService/IAssemblyService.cs
+++ Tharga.Toolkit/TypeService/IAssemblyService.cs
@@ -10,14 +10,14 @@ public interface IAssemblyService
     /// Loads types into the cache.
     /// </summary>
     /// <param name="cacheKey">Unique key for the specific cache.</param>
-    /// <param name="predicate">Filter the types that should be loaded.</param>
-    /// <param name="assemblies">Provide what assemblies should be used to find types. By default all assemblies in the current app domain with the first part of the assembly name is used.</param>
-    void LoadTypes(string cacheKey, Func<TypeInfo, bool> predicate, IEnumerable<Assembly> assemblies = null);
+    /// <param name="filter">Filter the types that should be loaded.</param>
+    /// <param name="assemblies">Provide what assemblies should be used to find types. See 'GetAssemblies' in 'AssemblyService' for default behaviour.</param>
+    void LoadTypes(string cacheKey, Func<TypeInfo, bool> filter, IEnumerable<Assembly> assemblies = null);
 
     /// <summary>
-    /// Gets types from cache. If a predicate is provided the cache can be populated on the first call.
+    /// Gets types from cache. If a filter is provided the cache can be populated on the first call.
     /// </summary>
     /// <param name="cacheKey">Unique key for the specific cache.</param>
-    /// <param name="predicate">Filter the types that should be loaded.</param>
-    TypeInfo[] GetTypes(string cacheKey, Func<TypeInfo, bool> predicate = null);
+    /// <param name="filter">Filter the types that should be loaded.</param>
+    TypeInfo[] GetTypes(string cacheKey, Func<TypeInfo, bool> filter = null);
 }
