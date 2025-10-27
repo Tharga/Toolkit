8a77a88a4f2da59b74d28f309105cd95addcfd5d
Daniel Bohlin <daniel.bohlin@live.se>
2025-10-27 13:39:35 +0100

    update namespace

--- Tharga.Toolkit.Standard/ByteSizeExtensions.cs
+++ Tharga.Toolkit.Standard/ByteSizeExtensions.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 
-public static class ByteSizeExtensions
+namespace Tharga.Toolkit
 {
-    private static readonly string[] ShortUnits = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
-    private static readonly string[] FullUnits = { "Bytes", "Kilobytes", "Megabytes", "Gigabytes", "Terabytes", "Petabytes", "Exabytes" };
-
-    public static string ToReadableByteSize(this int byteCount, bool useFullUnit = false, int decimalPlaces = 0)
+    public static class ByteSizeExtensions
     {
-        return ((long)byteCount).ToReadableByteSize(useFullUnit, decimalPlaces);
-    }
+        private static readonly string[] ShortUnits = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+        private static readonly string[] FullUnits = { "Bytes", "Kilobytes", "Megabytes", "Gigabytes", "Terabytes", "Petabytes", "Exabytes" };
 
-    public static string ToReadableByteSize(this long byteCount, bool useFullUnit = false, int decimalPlaces = 0)
-    {
-        if (byteCount < 0) throw new ArgumentOutOfRangeException(nameof(byteCount), "Byte count cannot be negative.");
+        public static string ToReadableByteSize(this int byteCount, bool useFullUnit = false, int decimalPlaces = 0)
+        {
+            return ((long)byteCount).ToReadableByteSize(useFullUnit, decimalPlaces);
+        }
 
-        if (byteCount == 0) return $"0 {(useFullUnit ? FullUnits[0] : ShortUnits[0])}";
+        public static string ToReadableByteSize(this long byteCount, bool useFullUnit = false, int decimalPlaces = 0)
+        {
+            if (byteCount < 0) throw new ArgumentOutOfRangeException(nameof(byteCount), "Byte count cannot be negative.");
 
-        var unitIndex = (int)Math.Floor(Math.Log(byteCount, 1024));
-        unitIndex = Math.Min(unitIndex, ShortUnits.Length - 1);
+            if (byteCount == 0) return $"0 {(useFullUnit ? FullUnits[0] : ShortUnits[0])}";
 
-        var adjustedSize = byteCount / Math.Pow(1024, unitIndex);
-        var format = $"F{decimalPlaces}";
+            var unitIndex = (int)Math.Floor(Math.Log(byteCount, 1024));
+            unitIndex = Math.Min(unitIndex, ShortUnits.Length - 1);
 
-        var unit = useFullUnit ? FullUnits[unitIndex] : ShortUnits[unitIndex];
+            var adjustedSize = byteCount / Math.Pow(1024, unitIndex);
+            var format = $"F{decimalPlaces}";
 
-        return $"{adjustedSize.ToString(format)} {unit}";
+            var unit = useFullUnit ? FullUnits[unitIndex] : ShortUnits[unitIndex];
+
+            return $"{adjustedSize.ToString(format)} {unit}";
+        }
     }
 }
