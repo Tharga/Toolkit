3472d92d641304246695c21e2c10c1696854fccd
Daniel Bohlin <daniel.bohlin@live.se>
2025-11-01 15:00:10 +0100

    extract test toolkit to separate repo

--- Tharga.Test.Toolkit/IAssignmentIssue.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tharga.Test.Toolkit
-{
-    public interface IAssignmentIssue
-    {
-        string Message { get; }
-        string ObjectName { get; }
-        int? Index { get; }
-    }
-}
